0b4cf420d18c83eaba7ef4c4841a23e20b005ef9
NeilChen20 <chenweining960227@gmail.com>
2022-06-01 16:10:22 +0800

    fix: Home.vue change HomeView.vue

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,15 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#000000",
+                secondary: "#484848",
+                gray: "#8D8D8D",
+                white: "#FFFFFF",
+                background: "#EFEFEF"
+            }
+        }
+    }
 });
