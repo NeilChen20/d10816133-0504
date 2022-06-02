84d1c3bcb165037d5b7ca164c1b3cb6149afa577
Neil <chenweining960227@gmail.com>
2022-06-02 22:29:44 +0800

    fix: set grid & title

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,13 @@
 <template> <!--標籤內放的是此頁面的畫面-->
-<div></div>
+<v-container>
+  <v-row>
+    <v-col cols="12">
+      <h1 class="text-center py-10">
+        人員清單
+        </h1>
+    </v-col>
+  </v-row>
+</v-container>
 </template>
 <script> /*coding JS，可使用vue api()，是指vue提供的func()，data()其中之一*/
 export default {
