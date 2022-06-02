106f8de9855b6ab512003224342953de732f7ce3
Neil <chenweining960227@gmail.com>
2022-06-03 00:06:03 +0800

    fix: v-card of v-for

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,37 +1,37 @@
 <template> <!--標籤內放的是此頁面的畫面-->
-<v-container>
-  <v-row> <!--portal-card1標題-->
-    <v-col cols="12">
-      <h1 class="text-center py-10">人員清單</h1>
-    </v-col>
-  </v-row>
-  <v-row> <!--portal-card2資料-->
-    <v-col md="4" cols="12" v-for="i in 9" :key="i">
-      <v-card v-for="i in 9" :key="i" height="150px" 
+  <v-container>
+    <v-row> <!--portal-card1標題-->
+      <v-col cols="12">
+        <h1 class="text-center py-10">人員清單</h1>
+      </v-col>
+    </v-row>
+    <v-row> <!--portal-card2資料-->
+      <v-col md="4" cols="12" v-for="i in 9" :key="i">
+        <v-card height="150px" 
       class="d-flex flex-column px-5 py-3 rounded-lg"> <!--rounded-lg調整v-card圓角-->
-        <div class="d-flex align-center flex-grow-0"> <!--name&sex--> <!--flex-grow比例分配--> <!--align-center水平對齊-置中-->
-          <h3 class="primary--text">
-            <strong>{{user.name}}</strong>
-          </h3>
-          <h4 class="gray--text ml-auto">{{user.gender}}</h4>
-        </div>
-        <div class="flex-grow-0"> <!--num-->
-          <span class="gray--text">{{user.UID}}</span>
-        </div>
-        <div class="d-flex align-end flex-grow-1"> <!--mail--> <!--align-end水平置中-下-->
-          <v-icon left color="secondary">mdi-gmail</v-icon>
-          <span class="secondary--text">{{user.email}}</span>
-        </div>
-      </v-card>
-    </v-col>
-  </v-row>
-  <v-row class="mt-8"> <!--換頁選單--> <!--mt-x頁數與上列資料的距離-->
-    <v-col cols="12">
-      <v-pagination :value="1" :length="6"/>
-    </v-col>
-  </v-row>
+          <div class="d-flex align-center flex-grow-0"> <!--name&sex--> <!--flex-grow比例分配--> <!--align-center水平對齊-置中-->
+            <h3 class="primary--text">
+              <strong>{{user.name}}</strong>
+            </h3>
+            <h4 class="gray--text ml-auto">{{user.gender}}</h4>
+          </div>
+          <div class="flex-grow-0"> <!--num-->
+            <span class="gray--text">{{user.UID}}</span>
+          </div>
+          <div class="d-flex align-end flex-grow-1"> <!--mail--> <!--align-end水平置中-下-->
+            <v-icon left color="secondary">mdi-gmail</v-icon>
+            <span class="secondary--text">{{user.email}}</span>
+          </div>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-row class="mt-8"> <!--換頁選單--> <!--mt-x頁數與上列資料的距離-->
+      <v-col cols="12">
+        <v-pagination :value="1" :length="6"/>
+      </v-col>
+    </v-row>
 
-</v-container>
+  </v-container>
 </template>
 <script> /*coding JS，可使用vue api()，是指vue提供的func()，data()是其一*/
 export default {
