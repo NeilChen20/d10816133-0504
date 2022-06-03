c81f91b8d6fc9533d38abd94121a4fe10ead8ae3
Neil <chenweining960227@gmail.com>
2022-06-03 23:39:55 +0800

    fix: set current data in for loop

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,12 @@
-<template> <!--標籤內放的是此頁面的畫面-->
+<template>  <!--標籤內放的是此頁面的畫面-->
   <v-container>
-    <v-row> <!--portal-card1標題-->
+    <v-row>  <!--portal-card1標題-->
       <v-col cols="12">
         <h1 class="text-center py-10">人員清單</h1>
       </v-col>
     </v-row>
-    <v-row> <!--portal-card2資料-->
-      <v-col md="4" cols="12" v-for="i in 9" :key="i">
+    <v-row>  <!--portal-card2資料-->
+      <v-col md="4" cols="12" v-for="user in currentUsers" :key="user.UID">
         <v-card height="150px" 
       class="d-flex flex-column px-5 py-3 rounded-lg"> <!--rounded-lg調整v-card圓角-->
           <div class="d-flex align-center flex-grow-0"> <!--name&sex--> <!--flex-grow比例分配--> <!--align-center水平對齊-置中-->
@@ -25,22 +25,42 @@
         </v-card>
       </v-col>
     </v-row>
-    <v-row class="mt-8"> <!--換頁選單--> <!--mt-x頁數與上列資料的距離-->
+    <v-row class="mt-8">  <!--換頁選單--> <!--mt-x頁數與上列資料的距離-->
       <v-col cols="12">
-        <v-pagination :value="1" :length="6"/>
+        <v-pagination v-model="page" :length="totalPages"/>
       </v-col>
     </v-row>
 
   </v-container>
 </template>
-<script> /*coding JS，可使用vue api()，是指vue提供的func()，data()是其一*/
+<script>  /*coding JS，可使用vue api()，是指vue提供的func()，data()是其一*/
 import admin from "../assets/admin.json";
 export default {
-data() {
-return {
-  
-};
-}
+  data() {
+    return {
+      users: [],
+      page:1,
+    };
+  },
+  created() {  /*進入畫面前獲取資料*/
+    this.getUsers();
+  },
+  methods: {  /*執行獲取資料並將資料放入*/
+    getUsers() {
+      this.users = admin.data.user;
+    }
+  },
+  computed: { /*換頁改變資料呈現*/
+    totalPages() {  /*計算總頁數*/
+      return Math.ceil(this.users.length / 9);
+    },
+    currentUsers() {  /*計算目前頁面使用者們*/
+      const begin = (this.page -1) * 9;
+      const end = begin + 9;
+      return this.users.slice(begin, end);
+    }
+  },
+
 };
 </script>
 <style scoped> /*此頁面的css*/
